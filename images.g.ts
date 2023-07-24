a0b505c7eef1c959649d1accc5397d456ad0e52a
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-07-24 13:07:50 -0400

    Restore '7/24/2023, 1:01:33 PM Update pxt.json'

---
+++ images.g.ts
@@ -0,0 +1,48 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "cursor_pixel":return img`
+3 
+`;
+            case "image2":
+            case "cursor_image":return img`
+f . . . . . . . . . 
+f f . . . . . . . . 
+f 1 f . . . . . . . 
+f 1 1 f . . . . . . 
+f 1 1 1 f . . . . . 
+f 1 1 1 1 f . . . . 
+f 1 1 1 1 1 f . . . 
+f 1 1 1 1 1 1 f . . 
+f 1 1 1 1 1 1 1 f . 
+f 1 1 f 1 f f f f f 
+f 1 f f 1 f . . . . 
+f f . . f 1 f . . . 
+f . . . f 1 f . . . 
+. . . . . f 1 f . . 
+. . . . . f 1 f . . 
+. . . . . . f . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
