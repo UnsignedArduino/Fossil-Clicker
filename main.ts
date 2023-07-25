ea65886ea7154032ed465465c2934c2b56e0f834
Cyrus Yiu <UnsignedArduino@outlook.com>
2023-07-24 21:46:07 -0400

    Menu cancel works

--- main.ts
+++ main.ts
@@ -23,12 +23,14 @@ function create_label (text: string, top: number, left: number) {
     return local_text_sprite
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
-    if (sprite_main_icon.overlapsWith(sprite_cursor)) {
-        click_main_icon()
-    } else {
-        for (let value of text_sprites_towers) {
-            if (sprite_main_icon.overlapsWith(sprite_cursor)) {
-            	
+    if (cursor_enabled) {
+        if (sprite_main_icon.overlapsWith(sprite_cursor)) {
+            click_main_icon()
+        } else {
+            for (let value of text_sprites_towers) {
+                if (sprite_cursor.overlapsWith(value)) {
+                    show_tower_menu([value])
+                }
             }
         }
     }
@@ -42,20 +44,42 @@ function short_scale_divider (num: number) {
     return 0
 }
 function create_towers () {
+    text_sprites_towers = []
     create_tower("Assistant", 0.1, 34, 48, assets.image`assistant_icon`, assets.image`assistant_icon_selected`, 10)
     create_tower("Paleontologist", 0.5, 56, 48, assets.image`paleontologist_icon`, assets.image`paleontologist_icon_selected`, 50)
 }
 function show_tower_menu (tower_in_list: TextSprite[]) {
-    enable_cursor(false, false)
+    enable_cursor(false)
     local_text_sprite = tower_in_list[0]
-    menu_items_tower = [miniMenu.createMenuItem("Buy 1 (" + format_money(calculate_buy_price(tower_in_list, 1)) + ")"), miniMenu.createMenuItem("Buy...")]
+    menu_items_tower = [miniMenu.createMenuItem("Cancel"), miniMenu.createMenuItem("Buy 1 (" + format_money(calculate_buy_price(tower_in_list, 1)) + ")"), miniMenu.createMenuItem("Buy...")]
     if (sprites.readDataNumber(local_text_sprite, "count") > 0) {
         menu_items_tower.push(miniMenu.createMenuItem("Sell 1 (" + format_money(calculate_sell_price(tower_in_list, 1)) + ")"))
         menu_items_tower.push(miniMenu.createMenuItem("Sell..."))
     }
     menu_tower = miniMenu.createMenuFromArray(menu_items_tower)
     menu_tower.setTitle(sprites.readDataString(local_text_sprite, "name"))
-    sprites.setDataSprite(local_text_sprite, "opened_menu", menu_tower)
+    menu_tower.left = local_text_sprite.left + 22
+    menu_tower.top = local_text_sprite.top
+    menu_tower.setDimensions(88, 42)
+    menu_tower.setMenuStyleProperty(miniMenu.MenuStyleProperty.Border, 1)
+    menu_tower.setMenuStyleProperty(miniMenu.MenuStyleProperty.BorderColor, images.colorBlock(15))
+    menu_tower.setStyleProperty(miniMenu.StyleKind.Title, miniMenu.StyleProperty.Foreground, images.colorBlock(1))
+    menu_tower.setStyleProperty(miniMenu.StyleKind.Title, miniMenu.StyleProperty.Background, images.colorBlock(15))
+    menu_tower.setStyleProperty(miniMenu.StyleKind.Selected, miniMenu.StyleProperty.Background, images.colorBlock(14))
+    menu_tower.onButtonPressed(controller.A, function (selection, selectedIndex) {
+        if (selectedIndex == 0) {
+            sprites.destroy(menu_tower)
+            timer.background(function () {
+                pauseUntil(() => !(controller.A.isPressed()))
+                enable_cursor(true)
+            })
+        }
+    })
+    menu_tower.setButtonEventsEnabled(false)
+    timer.background(function () {
+        pauseUntil(() => !(controller.A.isPressed()))
+        menu_tower.setButtonEventsEnabled(true)
+    })
 }
 function create_main_icon () {
     sprite_main_icon = sprites.create(assets.image`shovel`, SpriteKind.Player)
@@ -85,7 +109,7 @@ function calculate_sell_price (tower_in_list: Sprite[], count: number) {
 }
 function update_tower_button (text_sprite_in_list: TextSprite[]) {
     local_text_sprite = text_sprite_in_list[0]
-    local_text_sprite.setText(" " + sprites.readDataString(local_text_sprite, "name"))
+    local_text_sprite.setText(sprites.readDataString(local_text_sprite, "name"))
     if (sprite_cursor.overlapsWith(local_text_sprite)) {
         local_text_sprite.setIcon(sprites.readDataImage(local_text_sprite, "icon_hover"))
     } else {
@@ -110,13 +134,14 @@ function click_main_icon () {
     big_icon_until = game.runtime() + 100
     money += fossil_price
 }
-function enable_cursor (en: boolean, show: boolean) {
+function enable_cursor (en: boolean) {
     if (en) {
         controller.moveSprite(sprite_cursor)
     } else {
         controller.moveSprite(sprite_cursor, 0, 0)
     }
-    sprite_cursor_image.setFlag(SpriteFlag.Invisible, !(show))
+    sprite_cursor_image.setFlag(SpriteFlag.Invisible, !(en))
+    cursor_enabled = en
 }
 function calculate_buy_price (tower_in_list: Sprite[], count: number) {
     local_sprite = tower_in_list[0]
@@ -127,9 +152,6 @@ function calculate_buy_price (tower_in_list: Sprite[], count: number) {
     return local_sum
 }
 function create_tower (name: string, speed: number, top: number, left: number, icon: Image, icon_hover: Image, price: number) {
-    if (!(text_sprites_towers)) {
-        text_sprites_towers = []
-    }
     local_text_sprite = textsprite.create("", 0, 15)
     local_text_sprite.setKind(SpriteKind.Tower)
     local_text_sprite.setFlag(SpriteFlag.Ghost, false)
@@ -150,13 +172,14 @@ let local_sprite: Sprite = null
 let sprite_cursor_image: Sprite = null
 let menu_tower: miniMenu.MenuSprite = null
 let menu_items_tower: miniMenu.MenuItem[] = []
+let text_sprites_towers: TextSprite[] = []
 let sprite_cursor: Sprite = null
 let sprite_main_icon: Sprite = null
+let cursor_enabled = false
 let local_text_sprite: TextSprite = null
 let text_sprite_fossils_per_second: TextSprite = null
 let text_sprite_fossil_price: TextSprite = null
 let text_sprite_money: TextSprite = null
-let text_sprites_towers: TextSprite[] = []
 let short_scale_names: string[] = []
 let fossils_per_second = 0
 let fossil_price = 0
@@ -194,9 +217,8 @@ short_scale_names = [
 scene.setBackgroundColor(14)
 scene.setBackgroundImage(assets.image`background`)
 create_cursor()
-enable_cursor(true, true)
+enable_cursor(true)
 create_ui()
-show_tower_menu([text_sprites_towers[0]])
 game.onUpdate(function () {
     sprite_cursor_image.top = sprite_cursor.top
     sprite_cursor_image.left = sprite_cursor.left
